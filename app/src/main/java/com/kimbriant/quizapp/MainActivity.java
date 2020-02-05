f0ce55ece4b275c8ceaf868023bff3a947f62572
OctonianCat <melcalctheta@gmail.com>
2020-02-05 15:57:05 -0500

    2/5/20

--- app/src/main/java/com/kimbriant/quizapp/MainActivity.java
+++ app/src/main/java/com/kimbriant/quizapp/MainActivity.java
@@ -1,36 +1,59 @@
 package com.kimbriant.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
-    EditText textEdit;
-    @Override
+    EditText textEditName;
+    EditText notificationNumber;
+    TextView textView;
+    String mainActivity;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton = findViewById(R.id.clickButton);
-        textEdit = findViewById(R.id.responseEditText);
+        textEditName = findViewById(R.id.responseEditTextName);
+        textView = findViewById(R.id.textBox);
+        notificationNumber = findViewById(R.id.notificationNumber);
+        mainActivity = "MainActivity";
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Hello mom!");
-                Log.i("textButton", "Hi dad!" + textEdit.getText());
+                Log.i("textButton", "Hi dad!" + textEditName.getText());
             }
         });
-        textEdit.setText(textEdit.getText().toString() + " " + textEdit.getText().toString());
-        textEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        textEditName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus) {
-                    textEdit.setText("Ahhhhh");
+                String textEditInput = textEditName.getText().toString();
+                if(!hasFocus && textEditInput.equals("TJ")) {
+                    System.out.println("Lost focus of textEditName");
+                    textView.setText("TJ Rocks!");
+                    textEditName.setText("");
+                    textEditName.setHint("That's a gooooood name.");
                 }
+            }
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Wassup");
+                String currentTextEditInput = textEditName.getText().toString();
+                String hi = "hi";
+                textView.setText(hi + " " + currentTextEditInput);
+                String wassup = "Log wassup!";
+                Log.i(mainActivity, wassup);
+                Log.i(mainActivity, currentTextEditInput);
+                textEditName.clearFocus();
             }
         });
     }
